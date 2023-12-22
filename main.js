bed250700b4bcf29fbdbba484d84e661f98869f8
312310224 <154570729+312310224@users.noreply.github.com>
2023-12-22 10:24:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text",{
+    strings:["Sehat","Banyak Rejeki","Kurus"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
+const klikk = document.getElementById("klikk")
+
+function klik_saya(){
+    alert("JANGAN NGOPI BAEEEEEEE!!!")
+}
